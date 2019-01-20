2723950d85fc15f40196af1e026f4f87711b3892
mikeydavis <mikiedavis@hotmail.com>
2019-01-20 20:29:40 +0000

    Delete environment.js

--- env/environment.js
+++
@@ -1,8 +0,0 @@
-// TODO: replace if yours are different
-module.exports = {
-    accountName: 'mean-demo-cosmosdb',
-    databaseName: 'familiesdb', 
-    key: 'yUndgPV4JN8ZPINVBxCK7c6W1cOqjX1EsXqaPFWRb5wN6qKIoMT51NdE0J1ayBsFKrVUzw9QpRFG8fL6mEWttQ%3D%3D',
-    //key:'yUndgPV4JN8ZPINVBxCK7c6W1cOqjX1EsXqaPFWRb5wN6qKIoMT51NdE0J1ayBsFKrVUzw9QpRFG8fL6mEWttQ=='
-    port: 10255
-  };
